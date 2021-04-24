dd06a6f37b76c303c978e6b06d44576fd925a96d
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2021-04-24 11:22:18 +0300

    Додає ДЗ 2

---
+++ routes/api/contacts.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+const Contacts = require('../../model');
+const {
+  validationCreateContact,
+  validationUpdateContact,
+} = require('./valid-contact-router');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts();
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:contactId', async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId);
+    if (contact) {
+      return res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', validationCreateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body);
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        contact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:contactId', async (req, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
+    if (contact) {
+      return res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/:contactId', validationUpdateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.updateContact(
+      req.params.contactId,
+      req.body,
+    );
+
+    if (contact) {
+      return res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
